4e1ff0448f0d6376721e3ef4b3c48e90e6de4c32
Nikitha Medikonda <admin@Admins-MacBook-Pro-6.local>
2024-02-27 11:40:22 +0530

    division

--- area.java
+++ area.java
@@ -1,6 +1,7 @@
 class Main{
 public static void main(String[] args){
 int sum= int a+int b;
-int a=sum/2;git@github.com:NikithaMedikonda/Challenge2.git
+int a=sum/2;
+int a=sum/2;
 System.out.prinln("ARea is");}
 }
